718fec4d2925076df4869d4ea413ba136a2630ea
bondanjody <bondanjodys@gmail.com>
2023-02-27 13:09:55 +0700

    Menghapus file yang tidak diperlukan. Membuat komponen Header

---
+++ src/compoents/Layout/Header.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const Header = () => {
+    return <Fragment>
+        <header className="text-xl font-bold">
+            <h1>Bondan Web</h1>
+        </header>
+    </Fragment>
+}
+
+export default Header;
